f5a85dff9e1f5796e4652188159e675d1c943ee9
Raghv-Me <72394214+Raghv-Me@users.noreply.github.com>
2020-12-10 17:50:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,18 @@
 let deaths = 0,score = 0
+const getDeviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+      return "tablet";
+    }
+    if (
+      /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+        ua
+      )
+    ) {
+      return "mobile";
+    }
+    return "desktop";
+  };
 function draw(){
     
     
